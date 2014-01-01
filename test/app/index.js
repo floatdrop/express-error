8aecd390e0b078465092879d6f7bd8a980f6bf67
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-01 20:01:55 +0600

    Added html output

--- test/app/index.js
+++ test/app/index.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var app = express();
-var expressError = require('../..');
 
 app.get('/', function (req, res) {
     res.send('Hello World');
@@ -11,13 +10,6 @@ app.get('/', function (req, res) {
 app.get('/error', function (req, res) {
     // SECRETLINE
     require('./foobar');
-});
-
-app.use(expressError({contextLinesCount: 3}));
-
-app.use(function (err, req, res, next) {
-    res.statusCode = 500;
-    res.end(err.stack);
 });
 
 module.exports = app;
--- test/main.js
+++ test/main.js
@@ -1,19 +1,40 @@
-/* global describe, it, require */
+/* global describe, it, require, beforeEach */
 'use strict';
 var request = require('supertest');
-var assert = require('assert');
+var expressError = require('..');
 
-describe('GET /ping', function () {
+describe('main tests', function () {
+    beforeEach(function () {
+        delete require.cache[require.resolve('./app')];
+        this.app = require('./app');
+    });
+
     it('respond with OK', function (done) {
+        this.app.use(expressError({contextLinesCount: 3}));
         request(require('./app'))
             .get('/')
             .expect(200, done);
     });
 
     it('respond with ERROR', function (done) {
-        var app = require('./app');
-        request(app)
+        this.app.use(expressError({contextLinesCount: 3}));
+        this.app.use(function (err, req, res, next) {
+            res.statusCode = 500;
+            res.end(err.stack);
+        });
+        request(this.app)
             .get('/error')
             .expect(/SECRETLINE/g, done);
+    });
+
+    it('generates html', function (done) {
+        this.app.use(expressError({contextLinesCount: 3, sendHtml: true}));
+        this.app.use(function (err, req, res, next) {
+            res.statusCode = 500;
+            res.end(err.stack);
+        });
+        request(this.app)
+            .get('/error')
+            .expect(/<html/g, done);
     });
 });
